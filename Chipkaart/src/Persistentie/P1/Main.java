6043617470123f47bccf170c69dd1d9f17439b47
maarten <maarten.kuijf@student.hu.nl>
2021-09-16 14:05:35 +0200

    code geformatteerd

--- Chipkaart/src/Persistentie/P1/Main.java
+++ Chipkaart/src/Persistentie/P1/Main.java
@@ -5,17 +5,18 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        Connection mycon= DriverManager.getConnection("jdbc:postgresql://localhost/ovchip", "postgres", "Kaas");
-        Statement myStat= mycon.createStatement();
+        Connection mycon = DriverManager.getConnection("jdbc:postgresql://localhost/ovchip", "postgres", "Kaas");
+        Statement myStat = mycon.createStatement();
         ResultSet reiziger = myStat.executeQuery("select * from reiziger");
 
         System.out.println("Alle reizigers:");
-        while (reiziger.next()){
-            System.out.print( "#" + reiziger.getString("reiziger_id") + ": ");
+        while (reiziger.next()) {
+            System.out.print("#" + reiziger.getString("reiziger_id") + ": ");
             System.out.print(reiziger.getString("voorletters") + "." + " ");
-            if(reiziger.getString("tussenvoegsel") != null) {
+            if (reiziger.getString("tussenvoegsel") != null) {
                 System.out.print(reiziger.getString("tussenvoegsel") + " ");
             }
             System.out.print(reiziger.getString("achternaam") + " ");
